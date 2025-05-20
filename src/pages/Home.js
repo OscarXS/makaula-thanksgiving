1a638b8aace8523076c58948d9b530bc8a75f317
Oscar <oscar.sebeloane@doxim.com>
2025-05-20 20:52:58 +0200

    overflow fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -33,7 +33,7 @@ const Home = () => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={transition1}
-      className='section md:overflow-hidden'>
+      className='section lg:overflow-hidden'>
       <div className='container mx-auto h-full relative'>
         {/* text & image wrapper */}
         <div className='flex flex-col justify-center'>
@@ -45,7 +45,7 @@ const Home = () => {
             transition={transition1}
             onMouseEnter={mouseEnterHandler}
             onMouseLeave={mouseLeaveHandler}
-            className='text-center md:text-left w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start'
+            className='text-center lg:text-left w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start'
           >
             <h1 className='h1'>
               Makaula <br /> Family <br /> Thanksgiving
@@ -60,7 +60,7 @@ const Home = () => {
               animate={{ scale: 1 }}
               exit={{ scale: 0 }}
               transition={transition1}
-              className='relative lg:-right-36 overflow-hidden'>
+              className='relative lg:-right-36 lg:overflow-hidden'>
               <motion.img 
                 whileHover={{ scale: 1.1 }}
                 transition={transition1}
